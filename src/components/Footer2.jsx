b2aea1ad27af9aee77aa15e7e21e1d9956d162a1
CristiColtan <cristicoltan0202@yahoo.com>
2025-07-18 00:18:28 +0300

    (front) Try 2 to fix navbar

--- src/components/Footer2.jsx
+++ src/components/Footer2.jsx
@@ -25,7 +25,7 @@ const Footer2 = () => {
                 <iframe
                   title="Harta Footer2"
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2848.854168442359!2d25.976122408525132!3d44.436153545998074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b200e0aeff82e9%3A0x57ac84c4df0f9dc3!2sBulevardul%20Iuliu%20Maniu%20585%2C%20Bucure%C8%99ti%20061087!5e0!3m2!1sen!2sro!4v1739991044082!5m2!1sen!2sro"
-                  width="400"
+                  width="320"
                   height="300"
                   style={{ border: "0" }}
                   allowFullScreen=""
